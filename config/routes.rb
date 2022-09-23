1671672ceab0a07b6e238c76ef8a31890f882434
Ronald Tichauer <rtichauerv@gmail.com>
2022-09-23 12:01:26 -0300

    feat: default format json for api routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   scope '/api' do
-    get '/resources/:resource_id', to: 'resources#get_resource'
+    defaults format: :json do
+      get '/resources/:resource_id', to: 'api_resources#show'
+    end
   end
 
   resources :resources, only: %i[show] do
