74c6d1da35f8890e71fbcb3401854bb29f1b27d3
Juan Santos <jjsantosochoa@outlook.com>
2022-09-26 16:17:40 -0300

    test(feat/endpoint_comments): add test to check if return empty array if no comments

--- spec/integration/api_resource_comments_controller_spec.rb
+++ spec/integration/api_resource_comments_controller_spec.rb
@@ -4,6 +4,7 @@ describe ApiResourceCommentsController do
   let(:user) { create(:user) }
   let(:resource) { create(:resource, user:) }
   let(:resource_id) { resource.id }
+  let(:resource_comment) { create(:resource_comment, resource:, user:) }
 
   before do
     sign_in user
@@ -11,7 +12,7 @@ describe ApiResourceCommentsController do
 
   path '/api/resources/{resource_id}/resource_comments' do
     get 'Retrieves a list of resource comments' do
-      tags 'resource_comments'
+      tags 'Resource Comments'
       operationId 'getResourceComments'
       produces 'application/json'
       parameter name: :resource_id, in: :path, type: :string
@@ -27,6 +28,29 @@ describe ApiResourceCommentsController do
                  }
                }
         run_test!
+
+        context 'when resource have a comment' do
+          before do |example|
+            resource_comment.save
+            submit_request(example.metadata)
+          end
+
+          it 'returns one element' do
+            data = JSON.parse(response.body)
+            expect(data.length).to eq(1)
+          end
+        end
+
+        context 'when resource have no comments' do
+          before do |example|
+            submit_request(example.metadata)
+          end
+
+          it 'returns no elements' do
+            data = JSON.parse(response.body)
+            expect(data.length).to eq(0)
+          end
+        end
       end
 
       response '404', 'Resource Not Found' do
