095a9e0a6b7d261b068af05c79a09be9924e719f
Juan Santos <jjsantosochoa@outlook.com>
2022-09-26 17:08:11 -0300

    refactor(feat/endpoint_comments): add oneliner to get comments Also generate swagger.json with changes on spec

--- app/controllers/api_resource_comments_controller.rb
+++ app/controllers/api_resource_comments_controller.rb
@@ -1,8 +1,7 @@
 class ApiResourceCommentsController < ApiApplicationController
   def show
     resource_id = params['resource_id']
-    resource = Resource.find(resource_id)
-    comments = resource.resource_comments
+    comments = Resource.find(resource_id)&.resource_comments
     render json: comments, only: %i[id content resource_id user_id]
   end
 end
