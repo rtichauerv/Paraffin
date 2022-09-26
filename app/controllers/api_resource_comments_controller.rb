aea7944d110708da508157a84d06f5ec81bcda8b
ignacio <iaheck@uc.cl>
2022-09-26 12:00:10 -0300

    feat(createComments): controller receives json with content, inherits over ApiAppController and renders back a JSON with the comment created and its ID

--- app/controllers/api_resource_comments_controller.rb
+++ app/controllers/api_resource_comments_controller.rb
@@ -4,4 +4,20 @@ class ApiResourceCommentsController < ApiApplicationController
     comments = Resource.find(resource_id)&.resource_comments
     render json: comments, only: %i[id content resource_id user_id]
   end
+  
+  def create
+    content = params['comment']
+    resource_id = params['resource_id']
+    newComment = ResourceComment.new(
+      content:,
+      user: current_user,
+      resource_id:
+    )
+    newComment.save
+    render json: {
+      'commentId': newComment.id,
+      'content': newComment.content,
+      'resourceId': newComment.resource.id
+    }
+  end
 end
