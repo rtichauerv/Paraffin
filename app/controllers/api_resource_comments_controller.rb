7e91546f649e6793b065aa0ec4fe3e4c6a9b7946
Juan Santos <jjsantosochoa@outlook.com>
2022-09-26 16:18:20 -0300

    refactor(feat/endpoint_comments): refactor ApiResourceCommentsController

--- app/controllers/api_resource_comments_controller.rb
+++ app/controllers/api_resource_comments_controller.rb
@@ -2,7 +2,7 @@ class ApiResourceCommentsController < ApiApplicationController
   def show
     resource_id = params['resource_id']
     resource = Resource.find(resource_id)
-    comments = ResourceComment.where(resource:)
+    comments = resource.resource_comments
     render json: comments, only: %i[id content resource_id user_id]
   end
 end
