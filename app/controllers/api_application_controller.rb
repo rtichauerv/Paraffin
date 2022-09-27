83aa480fb2338310153d4ed20766b173a4ca0df4
ignacio <iaheck@uc.cl>
2022-09-27 14:29:48 -0300

    feat(createComments): refactor error handler and limit error granularity

--- app/controllers/api_application_controller.rb
+++ app/controllers/api_application_controller.rb
@@ -2,6 +2,7 @@ class ApiApplicationController < ActionController::API
   before_action :authenticate_user!
 
   rescue_from ActiveRecord::RecordNotFound, with: :handle_record_not_found_error
+  rescue_from ActiveRecord::RecordInvalid, with: :handle_record_invalid_error
 
   private
 
@@ -10,6 +11,18 @@ class ApiApplicationController < ActionController::API
     message = 'record_not_found'
     status = :not_found
 
+    render_error(code, message, status)
+  end
+
+  def handle_record_invalid_error
+    code = 400
+    status = :bad_request
+    message = 'invalid_record'
+
+    render_error(code, message, status)
+  end
+
+  def render_error(code, message, status)
     render json: { code:, message:, status: }, status:
   end
 end
--- app/controllers/api_resource_comments_controller.rb
+++ app/controllers/api_resource_comments_controller.rb
@@ -7,43 +7,12 @@ class ApiResourceCommentsController < ApiApplicationController
 
   def create
     content = params['content']
-    resource_id = params['resource_id']
-    comment = ResourceComment.new(content:, user: current_user, resource_id:)
-
-    if comment.save
-      render_successful_response(comment)
-    else
-      handle_save_error(content, resource_id)
-    end
-  end
-
-  private
-
-  def render_successful_response(new_comment)
-    status = :created
-    render json: {
-      'commentId': new_comment.id,
-      'content': new_comment.content,
-      'resource_id': new_comment.resource.id
-    }, status:
-  end
-
-  def handle_save_error(content, resource_id)
-    if content.nil?
-      handle_bad_request_custom_message('content_must_exist')
-    elsif content.strip == ''
-      handle_bad_request_custom_message('content_can_not_be_empty')
-    elsif Resource.where(id: resource_id).blank?
-      handle_record_not_found_error
-    else
-      handle_bad_request_custom_message('invalid_parameters')
-    end
-  end
-
-  def handle_bad_request_custom_message(message)
-    code = 400
-    status = :bad_request
-
-    render json: { code:, message:, status: }, status:
+    resource = Resource.find(params['resource_id'])
+    comment = ResourceComment.create!(
+      content:,
+      user: current_user,
+      resource_id: resource.id
+    )
+    render json: comment, only: %i[id content resource_id], status: :created
   end
 end
