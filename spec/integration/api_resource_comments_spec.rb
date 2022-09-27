e6ec0acc4ab6183ccb30b7f8cd9c017f576bdc88
ignacio <iaheck@uc.cl>
2022-09-27 11:25:42 -0300

    style(createComments): lint spaces auto corrections

--- spec/integration/api_resource_comments_spec.rb
+++ spec/integration/api_resource_comments_spec.rb
@@ -9,7 +9,7 @@ describe ApiResourceCommentsController do
   end
 
   describe 'Resources Comments API' do
-    path'/api/resources/{resource_id}/resource_comments' do
+    path '/api/resources/{resource_id}/resource_comments' do
       post 'Creates a new comment' do
         tags 'Resource comment'
         consumes 'application/json'
@@ -17,58 +17,60 @@ describe ApiResourceCommentsController do
         parameter name: :jsonBody, in: :body, schema: {
           type: :object,
           properties: {
-            content: {type: :string}
+            content: { type: :string }
           },
           required: ['content']
         }
         produces 'application/json'
         operationId 'createResourceComment'
-        
+
         response '201', 'Success' do
           schema type: :object,
-            properties: {
-              id: { type: :integer },
-              content: { type: :string },
-              resource_id: { type: :integer },
-            }
-          let(:resource_id){create(:resource).id}
-          let(:jsonBody){{'content': 'test'}}
+                 properties: {
+                   id: { type: :integer },
+                   content: { type: :string },
+                   resource_id: { type: :integer }
+                 }
+          let(:resource_id) { create(:resource).id }
+          let(:jsonBody) { { 'content': 'test' } }
           run_test!
         end
 
-        response 400, "Invalid parameters" do
+        response 400, 'Invalid parameters' do
           schema type: :object, properties: {
             'code': { type: :integer },
             'message': { type: :string },
             'status': { type: :string }
           }
-        
-          context "content must exist in json body" do
-            let(:resource_id){create(:resource).id}
-            let(:jsonBody) {{ 'asd': 'test' }}
+
+          context 'content must exist in json body' do
+            let(:resource_id) { create(:resource).id }
+            let(:jsonBody) { { 'asd': 'test' } }
+
             run_test!
           end
 
-          context "content can not be null" do
-            let(:resource_id){create(:resource).id}
-            let(:jsonBody) {{ 'content': '' }}
+          context 'content can not be null' do
+            let(:resource_id) { create(:resource).id }
+            let(:jsonBody) { { 'content': '' } }
+
             run_test!
           end
         end
 
-        response 404, "Resource does not exist" do
+        response 404, 'Resource does not exist' do
           schema type: :object, properties: {
             'code': { type: :integer },
             'message': { type: :string },
             'status': { type: :string }
           }
-          context "resource does not exist" do
+          context 'resource does not exist' do
             let(:resource_id) { 'invalid' }
-            let(:jsonBody){{'content': 'test'}}
+            let(:jsonBody) { { 'content': 'test' } }
+
             run_test!
           end
         end
-        
       end
     end
   end
