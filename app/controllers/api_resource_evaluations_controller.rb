8298b5e6ac41f81a71512b695b20a6508dbfc039
ristofer <cristopher.gomez@ug.uchile.cl>
2022-09-27 16:28:38 -0300

    feat(setResourceEvaluation): refactor of the ResourceEvaluationsController for error handling

--- app/controllers/api_resource_evaluations_controller.rb
+++ app/controllers/api_resource_evaluations_controller.rb
@@ -2,11 +2,12 @@ class ApiResourceEvaluationsController < ApiApplicationController
   def update
     evaluation = params['evaluation']
     resource_id = params['resource_id']
+    Resource.find(resource_id)
     resource_evaluation = ResourceEvaluation
                           .find_or_initialize_by(user_id: current_user.id,
                                                  resource_id:)
     resource_evaluation.evaluation = evaluation
-    resource_evaluation.save if resource_evaluation.has_changes_to_save?
-    render json: resource_evaluation
+    resource_evaluation.save!
+    render json: resource_evaluation, except: %i[created_at updated_at]
   end
 end
