6deb5d6535c9cef8d0fce846001d52cc61fa0286
Ronald Tichauer <rtichauerv@gmail.com>
2022-09-28 15:30:14 -0300

    feat: method for new endpoint

--- app/controllers/api_resources_controller.rb
+++ app/controllers/api_resources_controller.rb
@@ -9,4 +9,15 @@ class ApiResourcesController < ApiApplicationController
     render json: resources, only: %i[id name url],
            methods: [:average_evaluation]
   end
+
+  def create
+    learning_unit = LearningUnit.find(params[:learning_unit_id])
+    resource = Resource.create!(
+      name: params[:name],
+      url: params[:url],
+      learning_unit:,
+      user: current_user
+    )
+    render json: resource, only: %i[id name url], status: :created
+  end
 end
