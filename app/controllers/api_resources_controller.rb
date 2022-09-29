9774502ce4f3fcadfe268c74da42f48afa05013d
Vanessa Pulgar <vanessa.pulgardiez@gmail.com>
2022-09-29 17:31:07 -0300

    fix: typing errors

--- app/controllers/api_resources_controller.rb
+++ app/controllers/api_resources_controller.rb
@@ -3,15 +3,14 @@ class ApiResourcesController < ApplicationController
     resources = LearningUnit
                 .find(params[:learning_unit_id])
                 .resources
-    render json: resources, only: %i[,
-       name description]
+    render json: resources, only: %i[id name description]
   end
 
   def show
     resource = Resource
                .find(params[:resource_id])
 
-    render json: resource, only: %i[name description]
+    render json: resource, only: %i[id name description]
   end
 
   def create
@@ -23,31 +22,8 @@ class ApiResourcesController < ApplicationController
       learning_unit:,
       user: current_user
     )
-    render json: resource, only: %i[name url description], status: :created
+    render json: resource, only: %i[id name url description], status: :created
   end
-
-
-  def index_comments
-    comments = Resource
-               .find(params[:resource_id])
-               .resource_comments.includes(:user)
-
-    resource_comments = []
-    comments.each do |comment| 
-      json_hash = {
-        ,
-        : comment.,
-        , 
-        content: comment.content,
-        user_name: comment.user.name,
-        created_at: comment.created_at
-      }
-      resource_comments << json_hash  
-    end 
-    render json: resource_comments
-  end
-end
-
   def create_comments
     comment = ResourceComment.create!(
       content: params[:content],
