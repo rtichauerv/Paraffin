b0d89fb89da68852495c95d3a547eb1b1e7f44e1
Ronald Tichauer <rtichauerv@gmail.com>
2022-09-29 12:34:37 -0300

    fix: modified url

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
         to: 'api_completed_learning_units#'\
             'curriculums_learning_units_completed_by_user'
 
-      delete '/learning_units/:learning_unit_id/completed_learning_units',
+      delete '/learning_units/:learning_unit_id/completed_learning_unit',
         to: 'api_completed_learning_units#uncomplete_learning_unit'
 
       get '/current_session', to: 'api_session#index'
