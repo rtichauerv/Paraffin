00a9aa5676ed7de1ba5d45220005a836b805def8
Vanessa Pulgar <vanessa.pulgardiez@gmail.com>
2022-09-29 11:10:47 -0300

    fix: linter compliance

--- app/controllers/api_curriculums_controller.rb
+++ app/controllers/api_curriculums_controller.rb
@@ -3,6 +3,7 @@ class ApiCurriculumsController < ApiApplicationController
     curriculums = Curriculum.all
     render json: curriculums, only: %i[id name description]
   end
+
   def show
     curriculum = Curriculum.find(params[:curriculum_id])
     render json: curriculum, only: %i[id name description]
--- app/controllers/api_learning_units_controller.rb
+++ app/controllers/api_learning_units_controller.rb
@@ -3,10 +3,12 @@ class ApiLearningUnitsController < ApiApplicationController
     learning_units = Curriculum.find(params[:curriculum_id]).learning_units
     render json: learning_units, only: %i[id name]
   end
+
   def show
     learning_unit = Curriculum.find(params[:curriculum_id]).learning_units.find(params[:learning_unit_id])
-    #Para tener un path mas lindo
-    #learning_unit = Curriculum.find(params[:curriculum_id]).learning_units[(params[:learning_unit_id]).to_i]
+    # Para tener un path mas lindo
+    # learning_unit = Curriculum.find(params[:curriculum_id]).
+    # learning_units[(params[:learning_unit_id]).to_i]
     render json: learning_unit, only: %i[id name description]
   end
 end
