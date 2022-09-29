850ffdf406427a5bca3cdc34342af8af4cd48f52
cecyramallo <cecyramallo@gmail.com>
2022-09-29 10:43:56 -0300

    fix: fix 8 rubocop offenses

--- app/controllers/api_learning_units_controller.rb
+++ app/controllers/api_learning_units_controller.rb
@@ -1,6 +1,6 @@
 class ApiLearningUnitsController < ApiApplicationController
   def index
-    learning_units = LearningUnit.all()
+    learning_units = LearningUnit.all
     render json: learning_units, only: %i[id name]
   end
 end
