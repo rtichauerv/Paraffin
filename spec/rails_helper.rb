e3a60ade5372441218274a7d4d772e78ec7105d1
Vanessa Pulgar <vanessa.pulgardiez@gmail.com>
2022-10-03 16:05:26 -0300

    fix: rubocop compliance helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,9 +6,7 @@ require 'support/factory_bot'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
-if Rails.env.production?
-  abort('The Rails environment is running in production mode!')
-end
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
