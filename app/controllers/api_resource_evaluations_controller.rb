704369ab5fb914d7ab6cd7abdb871508115bbbe8
Juan Santos <jjsantosochoa@outlook.com>
2022-10-03 11:33:26 -0300

    refactor(feat/getResourceEvaluation): add find_by! for better error handling

--- app/controllers/api_resource_evaluations_controller.rb
+++ app/controllers/api_resource_evaluations_controller.rb
@@ -1,11 +1,8 @@
 class ApiResourceEvaluationsController < ApiApplicationController
   def show
     resource = Resource.find(params[:resource_id])
-    evaluation = resource.resource_evaluations.find_by(user: current_user)
-    if evaluation.nil?
-      handle_record_not_found_error
-    else
-      render json: evaluation, except: %i[created_at updated_at]
+    evaluation = resource.resource_evaluations.find_by!(user: current_user)
+    render json: evaluation, except: %i[created_at updated_at]
     end
   end
 
