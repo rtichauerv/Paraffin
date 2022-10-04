25d6dd6dc055ea663deb7b0354ea22ec1264bb32
cecyramallo <cecyramallo@gmail.com>
2022-10-04 15:57:25 -0300

    feat: create method for the index of completed learning units

--- app/controllers/api_learning_units_controller.rb
+++ app/controllers/api_learning_units_controller.rb
@@ -9,8 +9,15 @@ class ApiLearningUnitsController < ApiApplicationController
   def show
     learning_unit = Curriculum
                     .find(params[:curriculum_id])
-                    .learning_units
-                    .find(params[:learning_unit_id])
     render json: learning_unit, only: %i[id name description]
+  end
+
+  def index_completed_learning_units
+    completed_learning_units = Curriculum
+                               .find(params[:curriculum_id])
+                               .learning_units
+                               .find(params[:learning_unit_id])
+                               .completed_learning_units
+    render json: completed_learning_units
   end
 end
