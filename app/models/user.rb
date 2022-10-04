c4094dadbfa75a337d276b9a0edb253c36f7f92b
isavega <igvega02@gmail.com>
2022-10-04 14:34:40 -0300

    feat: add config for CSRF

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable, :validatable
 
   has_many :resources
   has_many :resource_comments
