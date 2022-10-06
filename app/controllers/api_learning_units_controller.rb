9650d1e37c20d89f88b82897e2fc85028c55ae95
Juan Santos <jjsantosochoa@outlook.com>
2022-10-06 12:27:18 -0300

    feat(getLearningUnit): creates getLearningUnit endpoint to retrieve a Learning Unit

--- app/controllers/api_learning_units_controller.rb
+++ app/controllers/api_learning_units_controller.rb
@@ -1,4 +1,9 @@
 class ApiLearningUnitsController < ApiApplicationController
+  def show
+    learning_unit = LearningUnit.find(params[:learning_unit_id])
+    render json: learning_unit, only: %i[id name]
+  end
+
   def learning_units_of_curriculum
     learning_units = Curriculum.find(params[:curriculum_id])&.learning_units
     render json: learning_units, only: %i[id name]
